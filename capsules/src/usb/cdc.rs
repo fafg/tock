81c4b8b7c4a1db06fafca6c7b9f863d539740d16
Brad Campbell <bradjc5@gmail.com>
2020-06-01 17:49:24 -0400

    cdc: set max pkt size to 64

--- capsules/src/usb/cdc.rs
+++ capsules/src/usb/cdc.rs
@@ -166,6 +166,7 @@ impl<'a, C: hil::usb::UsbController<'a>> Client<'a, C> {
                     product_string: 2,
                     serial_number_string: 3,
                     class: 0x2, // Class: CDC
+                    max_packet_size_ep0: 64,
                     ..descriptors::DeviceDescriptor::default()
                 },
                 descriptors::ConfigurationDescriptor {
